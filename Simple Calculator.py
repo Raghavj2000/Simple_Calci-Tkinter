1cba039b55d4fc891d9c0c9a722aa0c22230b885
Raghavendra J <89856193+Raghavj2000@users.noreply.github.com>
2022-09-10 13:52:48 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,64 @@
+import tkinter
+from tkinter import *
+from turtle import clear
+
+root=Tk()
+equa=""
+equation=StringVar()
+calculation=Label(root,textvariable=equation)
+equation.set("Enter Your math")
+calculation.grid(columnspan=4)
+def btnPress(num):
+    global equa
+    equa=equa+str(num)
+    equation.set(equa)
+    
+def EqualPress():
+    global equa
+    total=str(eval(equa))
+    equation.set(total)
+    equa=""
+
+def clear():
+    global equa
+    equa=""
+    equation.set("")
+
+button0=Button(root,text="0",command=lambda:btnPress(0))
+button0.grid(row=4,column=1)
+button1=Button(root,text="1",command=lambda:btnPress(1))
+button1.grid(row=1,column=0)
+button2=Button(root,text="2",command=lambda:btnPress(2))
+button2.grid(row=1,column=1)
+button3=Button(root,text="3",command=lambda:btnPress(3))
+button3.grid(row=1,column=2)
+button4=Button(root,text="4",command=lambda:btnPress(4))
+button4.grid(row=2,column=0)
+button5=Button(root,text="5",command=lambda:btnPress(5))
+button5.grid(row=2,column=1)
+button6=Button(root,text="6",command=lambda:btnPress(6))
+button6.grid(row=2,column=2)
+button7=Button(root,text="7",command=lambda:btnPress(7))
+button7.grid(row=3,column=0)
+button8=Button(root,text="8",command=lambda:btnPress(8))
+button8.grid(row=3,column=1)
+button9=Button(root,text="9",command=lambda:btnPress(9))
+button9.grid(row=3,column=2)
+
+Plus=Button(root,text="+",command=lambda:btnPress("+"))
+Plus.grid(row=1,column=4)
+Minus=Button(root,text="-",command=lambda:btnPress("-"))
+Minus.grid(row=2,column=4)
+Multiply=Button(root,text="x",command=lambda:btnPress("*"))
+Multiply.grid(row=3,column=4)
+Divide=Button(root,text="/",command=lambda:btnPress("/"))
+Divide.grid(row=4,column=4)
+
+Equal=Button(root,text="=",command=EqualPress)
+Equal.grid(row=4,column=2)
+Clear=Button(root,text="C",command=clear)
+Clear.grid(row=4,column=0)
+
+
+
+root.mainloop()
